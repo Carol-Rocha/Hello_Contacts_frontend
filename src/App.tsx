5d86d0c5595bdc6d4f88c2cee217ee383aac9cf4
Carol-Rocha <caroldsr1@gmail.com>
2023-09-20 08:34:34 -0300

    feat: create and config initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { RoutesMain } from './routes/RoutesMain'
 import { GlobalStyles } from './styles/Global'
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <RoutesMain />
     </>
   )
 }
