fd813a4ac05c27b31cd4d6aa0be8404ee79e37eb
Carol-Rocha <caroldsr1@gmail.com>
2023-10-03 20:11:50 -0300

    feat: create login component

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,42 @@
+import { useForm } from 'react-hook-form'
+import loginImage from '../../assets/images/Frame.svg'
+import closed_password from '../../assets/icons/closed_password.svg'
+import { TLoginData, loginSchema } from './validator'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useAuth } from '../../hooks/useAuth'
+
 export const LoginPage = () => {
-  return <h1>Login page</h1>
+  const { register, handleSubmit } = useForm<TLoginData>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  const { signIn } = useAuth()
+  return (
+    <main>
+      <section>
+        <div>
+          <h1>Login</h1>
+          <p>Login to your account</p>
+        </div>
+        <form onSubmit={handleSubmit(signIn)}>
+          <input type='email' placeholder='Email' {...register('email')} />
+          <input
+            type='password'
+            placeholder='Password'
+            {...register('password')}
+          />
+          <img src={closed_password} alt='icon' />
+          <div>
+            <button>Return</button>
+            <button>Login</button>
+          </div>
+        </form>
+        <div>
+          <span>Forgot your password?</span>
+          <span>Sign Up</span>
+        </div>
+      </section>
+      <img src={loginImage} alt='illustration image' />
+    </main>
+  )
 }
