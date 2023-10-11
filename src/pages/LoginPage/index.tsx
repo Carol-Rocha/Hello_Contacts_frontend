03175d00407e5a1457811c6fca60f69894e6f32f
Carol-Rocha <caroldsr1@gmail.com>
2023-10-10 22:29:22 -0300

    feat: create form component

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,42 +1,39 @@
 import { useForm } from 'react-hook-form'
-import loginImage from '../../assets/images/Frame.svg'
-import closed_password from '../../assets/icons/closed_password.svg'
 import { TLoginData, loginSchema } from './validator'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useAuth } from '../../hooks/useAuth'
+import { Input } from '../../components/Input'
+import { FormContainer } from '../../components/Form'
 
 export const LoginPage = () => {
-  const { register, handleSubmit } = useForm<TLoginData>({
+  const {
+    register,
+    handleSubmit
+    // formState: { errors }
+  } = useForm<TLoginData>({
     resolver: zodResolver(loginSchema)
   })
 
   const { signIn } = useAuth()
   return (
-    <main>
-      <section>
-        <div>
-          <h1>Login</h1>
-          <p>Login to your account</p>
+    <FormContainer
+      title='Login'
+      paragraph='Login to your account'
+      registration_question='Forgot your password?'
+      submitButton='Sign Up'
+    >
+      <form onSubmit={handleSubmit(signIn)}>
+        <Input type='email' placeholder='Email' register={register('email')} />
+        <Input
+          type='password'
+          placeholder='Password'
+          register={register('password')}
+        />
+        <div className='nav-buttons'>
+          <button>Return</button>
+          <button id='button-submit'>Login</button>
         </div>
-        <form onSubmit={handleSubmit(signIn)}>
-          <input type='email' placeholder='Email' {...register('email')} />
-          <input
-            type='password'
-            placeholder='Password'
-            {...register('password')}
-          />
-          <img src={closed_password} alt='icon' />
-          <div>
-            <button>Return</button>
-            <button>Login</button>
-          </div>
-        </form>
-        <div>
-          <span>Forgot your password?</span>
-          <span>Sign Up</span>
-        </div>
-      </section>
-      <img src={loginImage} alt='illustration image' />
-    </main>
+      </form>
+    </FormContainer>
   )
 }
