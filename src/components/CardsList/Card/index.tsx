7db9b0da4d4eb61174d85823e2cc5c9e29901777
Carol-Rocha <caroldsr1@gmail.com>
2023-10-14 14:44:51 -0300

    feat: add modal toggle functionality to edit contact

--- src/components/CardsList/Card/index.tsx
+++ src/components/CardsList/Card/index.tsx
@@ -4,15 +4,29 @@ import profile from '../../../assets/icons/profile.svg'
 import email from '../../../assets/icons/email.svg'
 import telephone from '../../../assets/icons/telephone.svg'
 import { IContact } from '../../../providers/ContactsProvider/types'
+import { useState } from 'react'
+import { Modal } from '../../Modal'
+import { ModalContainer } from '../../Modal/ModalContainer'
 
 interface IContactCardProps {
   contact: IContact
 }
 
 export const Card = ({ contact }: IContactCardProps) => {
+  const [isOpenEditContactModal, setIsOpenEditContactModal] =
+    useState<boolean>(false)
+
+  const toggleEditContactModal = () =>
+    setIsOpenEditContactModal(!isOpenEditContactModal)
+
   return (
     <StyledCard className='card'>
-      <img src={user_pen} alt='edit user icon' className='edit-icon' />
+      <img
+        onClick={toggleEditContactModal}
+        src={user_pen}
+        alt='edit user icon'
+        className='edit-icon'
+      />
       <div className='card-info'>
         <img src={profile} alt='profile icon' />
         <span>{contact.full_name}</span>
@@ -26,6 +40,16 @@ export const Card = ({ contact }: IContactCardProps) => {
         <span>{contact.telephone}</span>
       </div>
       <button>Delete</button>
+      {isOpenEditContactModal ? (
+        <Modal>
+          <ModalContainer
+            title='Update contact'
+            paragraph='Edit the contact information and press the Save button to save the changes'
+            submitButton='Save'
+            toggleModal={toggleEditContactModal}
+          />
+        </Modal>
+      ) : null}
     </StyledCard>
   )
 }
