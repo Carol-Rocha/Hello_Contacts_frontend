5e34c3d6c6def9f535178756d7ea33215cd1f042
Carol-Rocha <caroldsr1@gmail.com>
2023-10-20 12:16:32 -0300

    feat: create button component

--- src/components/Modal/ModalContainer/style.ts
+++ src/components/Modal/ModalContainer/style.ts
@@ -51,40 +51,6 @@ export const StyledModalContainer = styled.div`
         font-size: var(--font-size-12);
       }
     }
-
-    form {
-      .button-box {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        gap: 1rem;
-        padding: 1rem;
-
-        button {
-          padding: 1rem 1.5rem;
-          border: none;
-          border-radius: var(--border-radius-8);
-
-          background-color: var(--grey-1);
-          color: var(--grey-3);
-          font-size: var(--font-size-16);
-          font-weight: bold;
-        }
-
-        #button-submit {
-          background-color: var(--color-primary-1);
-          color: var(--grey-0);
-          font-weight: bold;
-        }
-
-        button:active,
-        #button-submit:active {
-          background-color: var(--grey-3);
-          color: var(--grey-0);
-          font-weight: bold;
-        }
-      }
-    }
   }
 
   /* Breakpoint para tablets maiores */
