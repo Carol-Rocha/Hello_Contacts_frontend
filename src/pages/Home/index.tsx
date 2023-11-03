05f4571e8ac8a57a2860e1a656b427df7ef39a89
Carol-Rocha <caroldsr1@gmail.com>
2023-11-02 21:59:05 -0300

    feat: create home page component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,68 @@
-import { LoginPage } from '../LoginPage'
+import { useForm } from 'react-hook-form'
+import homeImage from '../../assets/images/homeImage.svg'
+import { Input } from '../../components/Input'
+import { TLoginData, loginSchema } from '../LoginPage/validator'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useAuth } from '../../hooks/useAuth'
+import { StyledHomePage } from './style'
+import { StyledForm } from '../../styles/components/Form'
+import { StyledAskButton } from '../../styles/components/AskButtonComponent'
+import { handleButtonClick } from '../../utils/navigation'
 
 export const HomePage = () => {
-  return <div>Home</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<TLoginData>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  const { logIn } = useAuth()
+
+  return (
+    <StyledHomePage>
+      <div className='image-area hide-container'>
+        <img src={homeImage} alt='image' />
+      </div>
+      <section className='form-section'>
+        <div className='form-info'>
+          <h4>Hello Again!</h4>
+          <p>Check your contact list and register new contacts</p>
+        </div>
+        <StyledForm onSubmit={handleSubmit(logIn)}>
+          <Input
+            type='email'
+            placeholder='Email'
+            register={register('email')}
+          />
+          {errors.email ? (
+            <p className='error-message'>{errors.email.message}</p>
+          ) : null}
+          <Input
+            type='password'
+            placeholder='Password'
+            register={register('password')}
+          />
+          {errors.password ? (
+            <p className='error-message'>{errors.password.message}</p>
+          ) : null}
+
+          <StyledAskButton>
+            <span className='ask-button'>Forgot your password?</span>
+            <span className='link-button'>Recovery password</span>
+          </StyledAskButton>
+          <button id='button-submit' type='submit'>
+            Login
+          </button>
+        </StyledForm>
+        <StyledAskButton>
+          <span className='ask-button'>Do not have an account yet?</span>
+          <span onClick={handleButtonClick} className='link-button'>
+            Sign Up
+          </span>
+        </StyledAskButton>
+      </section>
+    </StyledHomePage>
+  )
 }
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+export const StyledHomePage = styled.main`
+  display: flex;
+
+  justify-content: center;
+
+  height: 100vh;
+  width: 100vw;
+
+  .hide-container {
+    display: none;
+  }
+
+  .image-area {
+    width: 50%;
+    background-color: var(--color-primary-1);
+    align-items: center;
+    img {
+      width: 700px;
+      height: 500px;
+    }
+  }
+
+  .form-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+
+    .form-info {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+
+      h4 {
+        color: var(--grey-6);
+        font-size: var(--font-size-26);
+        font-weight: var(--font-weight-400);
+      }
+
+      p {
+        color: var(--grey-3);
+        font-size: var(--font-size-16);
+        font-weight: var(--font-weight-400);
+      }
+    }
+  }
+
+  /* Breakpoint para tablets maiores */
+  @media (min-width: 600px) {
+    /* regras de estilo aqui */
+  }
+
+  /* Breakpoint para desktops pequenos */
+  @media (min-width: 800px) {
+  }
+
+  /* Breakpoint para desktops grandes */
+  @media (min-width: 1000px) {
+    flex-direction: row;
+    /* regras de estilo aqui */
+    .hide-container {
+      display: flex;
+    }
+  }
+`
