eb44077cf037c9d6a753eb77c0b2f5d372da2522
Carol-Rocha <caroldsr1@gmail.com>
2023-11-15 17:52:30 -0300

    fix: change global styling and global components

--- src/styles/components/MainContainer.ts
+++ src/styles/components/MainContainer.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const StyledMainContainer = styled.main`
   margin: 0 auto;
   align-items: center;
-  margin-top: 200px;
+  margin-top: 180px;
   width: 100%;
   max-width: 1200px;
   min-width: 344px;
